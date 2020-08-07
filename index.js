cb37c8e55ce6fd77ea9e83fea13e2daecdc3cec7
Christian <christianchrr@gmail.com>
2020-08-07 17:12:08 -0400

    Updates index to use OO classes

--- index.js
+++ index.js
@@ -1,31 +1,102 @@
-let patientContainer = document.getElementById('patient-container')
-let patientsPath = 'http:/localhost:3000/patients'
+const PatientContainer = document.getElementById('PatientContainer')
+const PatientData = document.getElementById('PatientData')
+const MedicationContainer = document.getElementById('MedicationContainer')
+const MedicationData = document.getElementById('MedicationData')
+const getPatientsPath = 'http://localhost:3000/patients/'
+const getMedicationsPath = 'http://localhost:3000/medications/'
 
-fetch(patientsPath)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(patientsArray){
-        patientsArray.forEach(function(patient){
-            patientContainer.innerHTML += makePatientDiv(patient)
+function getPatients() {
+    fetch(getPatientsPath)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(patientsArray){
+            patientsArray.forEach(function(patient){
+                return new Patient(patient)
+            })
+            Patient.renderAllPatients(PatientData);
+            MedicationContainer.style.display = 'none';
+            PatientContainer.style.display = 'block';
+        })
+}
+
+function getMedications() {
+    MedicationData.innerText = '';
+    fetch(getMedicationsPath)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(medicationsArray){
+            medicationsArray.forEach(function(medication){
+                return new Medication(medication)
+            })
+            Medication.renderAllMedications(MedicationData);
+            MedicationContainer.style.display = 'block';
+            PatientContainer.style.display = 'none';
+            populatePatientDropdown();
+        })
+}
+
+function populatePatientDropdown() {
+    let PatientDropdown = document.getElementById("PatientDropdown")
+    let length = PatientDropdown.options.length;
+    for (let i = length-1; i >= 0; i--) {
+        PatientDropdown.options[i] = null;
+    }
+    let defaultOption = document.createElement('option');
+    defaultOption.text = '-- Select --';
+    defaultOption.value = '';
+    PatientDropdown.add(defaultOption);
+    fetch(getPatientsPath)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(patientsArray){
+            patientsArray.forEach(function(patient){
+                let option = document.createElement('option');
+                option.text = patient.name;
+                option.value = patient.id;
+                PatientDropdown.add(option);
+            })
+        })
+}
+
+function submitPatient(e) {
+    e.preventDefault();
+
+    let patientName = document.getElementById("PatientName").value;
+    let patientAge = document.getElementById("PatientAge").value;
+
+    fetch('http://localhost:3000/patients', {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "name": patientName,
+            "age": patientAge
         })
     })
+}
 
-    function makePatientDiv(patient){
-        return `
-        <div id=patient-${patient.id}>
-            <p><span>${patient.name}, ${patient.age}</span></p>
-        </div>
-        `
-    }
+function submitMedication(e) {
+    e.preventDefault();
 
+    let medicationName = document.getElementById("MedicationName").value;
+    let medicationForm = document.getElementById("MedForm").value;
+    let medicationStrength = document.getElementById("MedicationStrength").value;
+    let patientId = document.getElementById("PatientDropdown").value;
 
-
-    // .then((response) => {
-    //     patients = response
-    //     let patientData = document.getElementById("Patients")
-    //     for (var i = 0; i < patients.length; i++) {
-    //         patientData.append(patients[i].name + ' - ')
-    //         patientData.append(patients[i].age + ', ')
-    //     }
-    // })
+    fetch('http://localhost:3000/medications', {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "name": medicationName,
+            "form": medicationForm,
+            "strength": medicationStrength,
+            "patient_id": patientId
+        })
+    })
+}
